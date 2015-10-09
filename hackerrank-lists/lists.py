b6cca9ecc77f29ce7efaa2a8ee32a5484cc2d7bf
Jessica Langston <jessica.langston11@gmail.com>
2015-10-09 13:23:43 -0700

    Completed HackerRank lists problem.

--- hackerrank-lists/lists.py
+++ hackerrank-lists/lists.py
@@ -1,6 +1,14 @@
 """ HackerRank practice problem for Python lists.
 
-Task: You have to initialize your list L = [] and follow the N commands given in N lines. Commands will be 1 of the 8 commands as given above, other than extend, and each command will have its value separated by space."""
+Task: You have to initialize your list L = [] and follow the N commands given in N lines. Commands will be 1 of the 8 commands as given above, other than extend, and each command will have its value separated by space.
+
+Using input.txt would return:
+[6, 5, 10]
+[1, 5, 9, 10]
+[9, 5, 1]
+
+
+"""
 
 input_file = open("input.txt")
 
@@ -11,24 +19,21 @@ N = int(input_data[0].strip())
 
 L = []
 
-# L = []
+for i in range(1, N + 1):
 
-# N = int(raw_input().strip())
+    line = input_data[i].split()
 
-# for i in range(N + 1):
-#     line = raw_input().strip().split()
-#     if line[0] == "insert":
-#         L.insert(int(line[1]), int(line[2]))
-#     elif line[0] == "print":
-#         print L
-#         L = []
-#     elif line[0] == "remove":
-#         L.remove(int(line[1])
-#     elif line[0] == "append":
-#         L.append(int(line[1]))
-#     elif line[0] == "sort":
-#         L.sort()
-#     elif line[0] == "pop":
-#         L.pop()
-#     else:
-#         L.reverse()
+    if line[0] == "insert":
+        L.insert(int(line[1]), int(line[2]))
+    elif line[0] == "remove":
+        L.remove(int(line[1]))
+    elif line[0] == "append":
+        L.append(int(line[1]))
+    elif line[0] == "sort":
+        L.sort()
+    elif line[0] == "pop":
+        L.pop()
+    elif line[0] == "reverse":
+        L.reverse()
+    else:
+        print L
